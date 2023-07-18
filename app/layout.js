708735da0391a12f53b768565a599b7d2c95fb03
Niraj <modiniraj1034@gmail.com>
2023-07-18 22:40:43 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 
+import Image from './opengraph-image'
+
 import { Inter } from 'next/font/google'
 import { NextAuthProvider } from './sessionProvider'
 import { Toaster } from 'react-hot-toast'
@@ -36,13 +38,15 @@ export default async function RootLayout({ children }) {
     <html lang="en">
       <head></head>
 
-      <body className={`${inter.className} box-border bg-[url('/Images/backGround.png')] bg-cover bg-opacity-50 bg-fixed bg-no-repeat h-[calc(100vh-20px)] m-0 p-0`} >
-      
+      <body className={`${inter.className} box-border bg-[url('/Images/backGround.png')] bg-cover bg-opacity-50 bg-fixed bg-no-repeat h-[calc(100vh-20px)] m-0 p-0 relative`} >
+
       <NextAuthProvider>
         <Toaster />
           <StateContext>
           <LayoutProvider>
+          <>
             <main className='md:pt-[61px] pt-[53px] h-[calc(100vh-200px)] md:h-[calc(100vh-60px)] md:overflow-hidden'>{children}</main>
+          </>
           </LayoutProvider>
           </StateContext>
         </NextAuthProvider>
